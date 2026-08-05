a627b121dfcabf482fdb7fc03f8d654716bbae81
Pham Tung Lam <fam.tung.lam@gmail.com>
2026-08-05 14:29:42 +0200

    chore(ptlam-skills): Remove superseded ptlam-visualization skill

--- skills/productivity/ptlam-visualization/scripts/mermaid/internal/command-error.mjs
+++
@@ -1,7 +0,0 @@
-export class MermaidCommandError extends Error {
-  constructor(code, message, details = undefined) {
-    super(message);
-    this.code = code;
-    this.details = details;
-  }
-}
